61d1fcbb592779b447da6d1180d0e2ab62f27dc8
Somkiat <somkiat.p@gmail.com>
2014-05-30 13:13:26 +0700

    Refactoring code :: recursive generate method in another case

--- src/main/java/kata/anagrams/Anagrams.java
+++ src/main/java/kata/anagrams/Anagrams.java
@@ -10,10 +10,10 @@ public class Anagrams {
 		if (input.length() == 3) {
 			result.add(input.substring(0, 1) + generate(convertData(input, 0)).get(0));
 			result.add(input.substring(0, 1) + generate(convertData(input, 0)).get(1));
-			result.add(input.substring(1, 2) + convertData("AC", 1));
-			result.add(input.substring(1, 2) + convertData("AC", 2));
-			result.add(input.substring(2, 3) + convertData("AB", 1));
-			result.add(input.substring(2, 3) + convertData("AB", 2));
+			result.add(input.substring(1, 2) + generate(convertData(input, 1)).get(0));
+			result.add(input.substring(1, 2) + generate(convertData(input, 1)).get(1));
+			result.add(input.substring(2, 3) + generate(convertData(input, 2)).get(0));
+			result.add(input.substring(2, 3) + generate(convertData(input, 2)).get(1));
 		} else if (input.length() == 2) {
 			result.add(input);
 			result.add(input.substring(1, 2) + input.substring(0, 1));
@@ -27,9 +27,10 @@ public class Anagrams {
 		if( round == 0 ) {
 			return "BC";
 		}
-		if (round == 2)
-			return input.substring(1, 2) + input.substring(0, 1);
-		return input;
+		if( round == 1 ) {
+			return "AC";
+		}
+		return "AB";
 	}
 
 }
