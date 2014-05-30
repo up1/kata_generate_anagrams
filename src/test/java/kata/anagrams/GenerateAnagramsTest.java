c64906be0674029163f4cf66d22d20cee630e9bc
Somkiat <somkiat.p@gmail.com>
2014-05-30 15:20:59 +0700

    Add new test case with biro

--- src/test/java/kata/anagrams/GenerateAnagramsTest.java
+++ src/test/java/kata/anagrams/GenerateAnagramsTest.java
@@ -36,6 +36,16 @@ public class GenerateAnagramsTest {
 		List<String> result = anagrams.generate("ABC");
 		assertEquals(list("ABC", "ACB", "BAC", "BCA", "CAB", "CBA"), result);
 	}
+	
+	@Test
+	public void Input_with_biro_character_Should_Return_24_output() {
+		Anagrams anagrams = new Anagrams();
+		List<String> result = anagrams.generate("biro");
+		assertEquals(list("biro", "bior", "brio", "broi", "boir", "bori"
+				,"ibro", "ibor", "irbo", "irob", "iobr", "iorb"
+				,"rbio", "rboi", "ribo", "riob", "robi", "roib"
+				,"obir", "obri", "oibr", "oirb", "orbi", "orib"), result);
+	}
 
 	private List<String> list(String... strings) {
 		List<String> list = new ArrayList<String>();
