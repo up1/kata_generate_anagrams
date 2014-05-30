304f1b4aa46073d7e30a496d78f42d2c774ed9c2
Somkiat <somkiat.p@gmail.com>
2014-05-30 09:47:31 +0700

    Refactoring with remove duplication and remove constant to variable

--- src/main/java/kata/anagrams/Anagrams.java
+++ src/main/java/kata/anagrams/Anagrams.java
@@ -8,8 +8,8 @@ public class Anagrams {
 	public List<String> generate(String input) {
 		List<String> result = new ArrayList<String>();
 		if (input.length() == 2) {
-			result.add("AB");
-			result.add("BA");
+			result.add(input);
+			result.add(input.substring(1, 2) + input.substring(0, 1));
 		} else {
 			result.add(input);
 		}
