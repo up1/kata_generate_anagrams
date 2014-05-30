a3a4e493ebfcf5603562aaa6bd31b48291509c90
Somkiat <somkiat.p@gmail.com>
2014-05-30 09:55:11 +0700

    Resolve with stupid solution

--- src/main/java/kata/anagrams/Anagrams.java
+++ src/main/java/kata/anagrams/Anagrams.java
@@ -7,7 +7,14 @@ public class Anagrams {
 
 	public List<String> generate(String input) {
 		List<String> result = new ArrayList<String>();
-		if (input.length() == 2) {
+		if (input.length() == 3) {
+			result.add(input);
+			result.add("ACB");
+			result.add("BAC");
+			result.add("BCA");
+			result.add("CAB");
+			result.add("CBA");
+		} else if (input.length() == 2) {
 			result.add(input);
 			result.add(input.substring(1, 2) + input.substring(0, 1));
 		} else {
